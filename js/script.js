51c621f888f661fea2385d6264b635af5e5aabb0
Greg Fedirko <gjfedirko@gmail.com>
2014-11-21 15:20:24 -0800

    jquery and bootstrap folders

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  var $bannerSection = $('.banner-section');
+
+  $(window).bind('scroll',function(e){
+      parallaxScroll($bannerSection, 0.25);
+      fadeScroll($bannerSection, 0, 250);
+    });
+ 
+    function parallaxScroll($element, dragCoefficient){
+      var scrolledY = $(window).scrollTop();
+      $element.css('top','-'+((scrolledY*dragCoefficient))+'px');
+    }
+
+    function fadeScroll($element, fadeStart, fadeUntil) {
+      var offset = $(document).scrollTop();
+      var opacity = 0;
+
+      if( offset <= fadeStart ) {
+          opacity=1;
+      } else if( offset<=fadeUntil ) {
+          opacity = 1 - (offset/fadeUntil);
+      }
+      $element.css('opacity', opacity);
+    }
+ 
+});
